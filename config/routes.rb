72d6c64ee029513dc1821771c6b9aa361a47050f
Morten Følling <hoensi@me.com>
2017-09-05 16:35:35 +0200

    Made a task manager w/ugly style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+resources :tasks
+#   get '/tasks'
+
+#   get 'tasks/show'
+
+#   get 'tasks/new'
+
+#   get 'tasks/create'
+
+#   get 'tasks/edit'
+
+#   get 'tasks/update'
+
+#   get 'tasks/destroy'
+
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(name: "this is a task", description: "but not really")
+Task.create(name: "Do the bootcamp", description: "but not really1")
+Task.create(name: "Get money", description: "but not really2")
